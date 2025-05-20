5e5498960684ae699d0ae073f2e95e087960e746
ethanelliot7 <ethanwaelliot@gmail.com>
2025-05-20 19:32:45 +1200

    improved race javadoc.

--- src/main/java/seng201/team019/models/Race.java
+++ src/main/java/seng201/team019/models/Race.java
@@ -14,26 +14,81 @@ import java.util.stream.Collectors;
  * Class representing a race.
  */
 public class Race {
-    private final static float RANDOM_EVENT_PERCENTAGE = 0.6f;
-    private final static float OPPONENT_DNF_PERCENTAGE = 0.2f;
+    /**
+     * The percentage change of a random event occurring in a race.
+     */
+    private static final float RANDOM_EVENT_PERCENTAGE = 0.6f;
 
+    /**
+     * The percentage change of an opponent not finishing in a race.
+     */
+    private static final float OPPONENT_DNF_PERCENTAGE = 0.2f;
+
+    /**
+     * The name of a race.
+     */
     private final String name;
+
+    /**
+     * A list of  {@link Route}'s the race has
+     */
     private final List<Route> routes;
+
+    /**
+     * The prize money earned from a race.
+     */
     private final float prizeMoney;
+
+    /**
+     * The duration of a race in milliseconds.
+     */
     private final long duration;
+
+    /**
+     * The number of opponents in a race
+     */
     private final int numOfOpponents;
 
+    /**
+     * A list of the {@link Opponent}'s in a race
+     */
     private List<Opponent> opponentCars;
+
+    /**
+     * The {@link Player} in a race.
+     */
     private Player player;
 
+    /**
+     * The current time of a race in milliseconds. Is in range 0 to duration.
+     */
     private long raceTime;
 
+    /**
+     * Indicates if the player has completed the race.
+     */
     private boolean isCompleted;
 
+    /**
+     * Indicates whether a random event will occur during the race.
+     */
     private boolean isEventScheduledThisRace;
+
+    /**
+     * Indicates whether a random event has occurred during the race.
+     */
     private boolean eventHasOccurred = false;
+
+    /**
+     * The time that a random event will occur during a race.
+     */
     private long eventTriggerTime = -1;
+
+    /**
+     * The {@link RandomEvent} that will occur during a race.
+     */
     RandomEvent selectedEvent = null;
+
 
     public Race(Builder builder) {
         this.name = builder.name;
@@ -89,6 +144,11 @@ public class Race {
         return player;
     }
 
+    /**
+     * Sets the player instance that is going to be playing in the race.
+     * If the random event is to pick up standard traveler we need to set the pickup time on the player object.
+     * @param player the {@link Player} instance that should be set
+     */
     public void setPlayer(Player player) {
         this.player = player;
         // If the RandomEvent is for the player picking up traveler
@@ -100,7 +160,7 @@ public class Race {
 
     /**
      * gets all the racers, including the player, involved in the race.
-     * @return list of all racers in race.
+     * @return list of all {@link Racer}'s in race.
      */
     public List<Racer> getRacers() {
         List<Racer> racers = new ArrayList<>(opponentCars);
@@ -116,6 +176,10 @@ public class Race {
         return duration;
     }
 
+    /**
+     * Increase the race time by delta
+     * @param delta the time increment in milliseconds.
+     */
     public void incrementRaceTime(long delta) {
         if (raceTime + delta >= duration) {
             raceTime = duration;
@@ -124,6 +188,11 @@ public class Race {
         }
     }
 
+    /**
+     * Indicates whether a random event should trigger
+     * Based on if its trigger time has passed and/or the event has already occurred.
+     * @return true if the event should trigger, false otherwise
+     */
     public boolean shouldTriggerRandomEvent() {
         if (isEventScheduledThisRace && !eventHasOccurred) {
             if (eventTriggerTime <= raceTime) {
@@ -134,6 +203,10 @@ public class Race {
         return false;
     }
 
+    /**
+     * Loops through the racers racing and increased their distance traveled in the time delta
+     * @param delta the time increment in milliseconds.
+     */
     public void updateRacers(long delta) {
         for (Racer racer : getRacers()) {
             float distanceTraveled = racer.simulateDriveByTime(delta);
@@ -141,6 +214,9 @@ public class Race {
         }
     }
 
+    /**
+     * Sets didDNF all racers that haven't finished to true.
+     */
     public void setDNFOfDurationExceedingRacers() {
         for (Racer racer : getRacers()) {
             if (racer.isFinished() || racer.didDNF())
@@ -155,13 +231,22 @@ public class Race {
         }
     }
 
+    /**
+     * Indicates whether the race is finished
+     * @return true if race is finished, false if race is not finished.
+     */
     public boolean isRaceFinished() {
         return getRacers().stream().filter((racer -> !racer.didDNF())).allMatch(Racer::isFinished) || raceTime >= duration;
     }
 
+    /**
+     * Returns the racers in order.
+     * Racers that have DNF are last,
+     * then sort by distance percentage with the highest completion first,
+     * then sort by finish time with the lowest time.
+     * @return ordered list of {@link Racer}'s
+     */
     public List<Racer> getOrderedRacers() {
-        // Dnf last, order by those with the most distance traveled, if there are
-        // multiple finishers order by Finish time.
         Comparator<Racer> raceOrderComparator = Comparator.comparing(Racer::didDNF).reversed()
                 .thenComparing((Racer racer) -> racer.getRoute().normalizeDistance(racer.getDistance())).reversed()
                 .thenComparing((Racer racer) -> racer.isFinished() ? racer.getFinishTime() : Long.MAX_VALUE);
@@ -169,6 +254,10 @@ public class Race {
         return getRacers().stream().sorted(raceOrderComparator).collect(Collectors.toList());
     }
 
+    /**
+     * Gets the finishing position of the player, if the player DNF the -1 is returned.
+     * @return player finishing position as an integer.
+     */
     public int getPlayerFinishedPosition() {
         if (player.didDNF()) {
             return -1;
@@ -176,6 +265,19 @@ public class Race {
         return 1 + getOrderedRacers().indexOf(player);
     }
 
+    /**
+     * Calculated the player profit from a race.
+     * player gets a fractional percentage of prizeMoney based on finishing position.
+     * Player gets $0 if they DNF.
+     * <p>
+     * e.g. For a race with 3 opponents where all racers finish:
+     * player gets 4/4 * prizeMoney if they come 1st.
+     * player gets 3/4 * prizeMoney if they come 2nd.
+     * player gets 2/4 * prizeMoney if they come 3rd.
+     * player gets 1/4 * prizeMoney if they come 4th.
+     *
+     * @return the money earned as a float
+     */
     public float getPlayerProfit() {
         if (getPlayer().didDNF()) {
             return 0;
@@ -194,10 +296,16 @@ public class Race {
         this.isCompleted = isCompleted;
     }
 
+    /**
+     * @return a Builder object for creating a new race instance.
+     */
     public static Builder builder() {
         return new Builder();
     }
 
+    /**
+     * Class for the Race Builder.
+     */
     public static class Builder {
         @JsonProperty(value = "name", required = true)
         private String name;
@@ -240,6 +348,12 @@ public class Race {
             return this;
         }
 
+
+        /**
+         * Constructs a new {@link Race} instance with the fields set.
+         * @return a new race instance
+         * @throws IllegalStateException if required fields (routes, prizeMoney, duration) are not set
+         */
         public Race build() {
             if (name == null) {
                 name = "Race!"; // give default name
